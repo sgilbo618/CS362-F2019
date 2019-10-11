d6fd8e3c4f9ee330ecefe3db7b3ec0c2251f52c9
sgilbo618@gmail.com <sgilbo618@gmail.com>
2019-10-11 08:29:11 -0400

    clean up discard/redraw in minionCardEffect()

--- projects/guilbeas/dominion/dominion.c
+++ projects/guilbeas/dominion/dominion.c
@@ -1251,42 +1251,26 @@ int minionCardEffect(struct gameState *state, int choice1, int choice2, int curr
 
 	discardCard(handPos, currentPlayer, state, 0); //discard card from hand
 
-	if (choice1)
+	if (choice1) //get +2 coins
 	{
 		state->coins = state->coins + 2;
 	}
 	else if (choice2) //discard hand, redraw 4, other players with 5+ cards discard hand and draw 4
 	{
-		//discard hand
-		while (numHandCards(state) > 0)
-		{
-			discardCard(handPos, currentPlayer, state, 0);
-		}
-
-		//draw 4
-		for (int i = 0; i < 4; i++)
-		{
-			drawCard(currentPlayer, state);
-		}
-
-		//other players discard hand and redraw if hand size > 4
 		for (int i = 0; i < state->numPlayers; i++)
 		{
-			if (i != currentPlayer)
+			if (i == currentPlayer || state->handCount[i] > 4)
 			{
-				if (state->handCount[i] > 4)
+				//discard hand
+				while (state->handCount[i] > 0)
 				{
-					//discard hand
-					while (state->handCount[i] > 0)
-					{
-						discardCard(handPos, i, state, 0);
-					}
+					discardCard(handPos, i, state, 0);
+				}
 
-					//draw 4
-					for (int j = 0; j < 4; j++)
-					{
-						drawCard(i, state);
-					}
+				//draw 4
+				for (int j = 0; j < 4; j++)
+				{
+					drawCard(i, state);
 				}
 			}
 		}
